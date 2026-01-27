7bb75d2b3f5831341e51694a50e8d26b101dd848
ui msmr <u@msmr.co>
2026-01-27 17:58:34 +0100

    small edit

--- nearby-spaces.js
+++ nearby-spaces.js
@@ -204,7 +204,7 @@ class NearbySpacesManager {
       <div class="nearby-header-row-top">
         <div class="settings-header-content">
           <i class="fas fa-map-marker-alt" style="color: var(--space-hover); margin-right: 6px;"></i>
-          <span class="nearby-header-text"><b>${currentSpaces.length}</b> ${makerspaceText}:</span>
+          <span class="nearby-header-text"><b>${currentSpaces.length}</b> ${makerspaceText}</span>
         </div>
         <button class="settings-icon-btn nearby-close-btn"><i class="fas fa-times"></i></button>
       </div>
